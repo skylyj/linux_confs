f995b1a8f17389669861b56c378c0a5fab12b350
lianyijiang <yijiang.lian@mintegral.com>
2023-02-26 11:34:49 +0800

    terminal下的菜单栏隐藏提交

--- emacs_personal/basic_conf.el
+++ emacs_personal/basic_conf.el
@@ -17,7 +17,9 @@
       (scroll-bar-mode -1)
       )
   (progn
-    (tool-bar-mode -1)
+    (menu-bar-mode -1) 
+    (toggle-scroll-bar -1) 
+    (tool-bar-mode -1) 
     )
   )
 (require 'dired-x)
